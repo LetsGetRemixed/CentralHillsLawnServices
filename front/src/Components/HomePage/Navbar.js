6f097e43631dae79fcd1283232c7fd4042524171
LetsGetRemixed <73035961+LetsGetRemixed@users.noreply.github.com>
2024-09-11 18:06:10 -0500

    Favicon and changes
    Favicon and changes

--- front/src/Components/HomePage/Navbar.js
+++ front/src/Components/HomePage/Navbar.js
@@ -5,18 +5,18 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="absolute left-0 w-full text-Bright-White border-white border-t-4 border-b-4 py-4 shadow-lg z-50" style={{ top: '80px', backgroundImage: `url('/images/Leather.jpg')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+    <nav className="absolute left-0 w-full text-Bright-White border-white border-t-4 border-b-4 py-4 shadow-lg z-50" style={{ top: '60px', backgroundImage: `url('/images/Leather.jpg')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
 
       <div className="container mx-auto flex justify-between items-center relative">
         {/* Logo */}
         <div className="absolute left-4 -top-18 flex items-center">
-          <div className="h-48 w-48 rounded-full bg-rustic-brown border-4 border-white flex items-center justify-center shadow-md">
-            <img src="/LoganLogo.png" alt="Logo" className="h-46 w-46 rounded-full" />
+          <div className="h-24 w-24 sm:h-48 sm:w-48 rounded-full bg-rustic-brown border-4 border-white flex items-center justify-center shadow-md">
+            <img src="/LoganLogo.png" alt="Logo" className="h-22 w-22 sm:h-46 sm:w-46 rounded-full" />
           </div>
         </div>
 
        {/* Desktop Menu Items */}
-       <div className="hidden md:flex flex-1 justify-center space-x-20 text-5xl font-header">
+       <div className="hidden md:flex flex-1 justify-end space-x-32 text-5xl font-header">
           <Link to="gallery" smooth={true} duration={500} className="cursor-pointer hover:text-sage-green">
             Gallery
           </Link>
@@ -72,7 +72,7 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       {isOpen && (
-        <div className="md:hidden bg-rustic-brown text-Bright-White px-6 text-center pt-4 pb-6 text-2xl font-header space-y-4">
+        <div className="md:hidden text-Bright-White px-6 text-center pt-4 pb-6 text-2xl font-header space-y-4" style={{ backgroundImage: `url('/images/Leather.jpg')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
           <Link
             to="gallery"
             smooth={true}
